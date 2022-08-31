c946efc60304f4eb50e6d88623264c8272fe7d7c
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2022-08-31 20:57:27 +0200

    Design Database and Migration

---
+++ Infrastructure/MiniECommerce.Persistance/DesignTimeDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using MiniECommerce.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniECommerce.Persistence
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<MiniECommerceDbContext>
+    {
+        public MiniECommerceDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<MiniECommerceDbContext> dbContextOptionsBuilder = new();
+            dbContextOptionsBuilder.UseNpgsql(Configuration.ConnectionString);
+            return new (dbContextOptionsBuilder.Options);
+        }
+    }
+}
--- Infrastructure/MiniECommerce.Persistance/ServiceRegistration.cs
+++ Infrastructure/MiniECommerce.Persistance/ServiceRegistration.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using MiniECommerce.Application.Abstractions;
 using MiniECommerce.Persistence.Concretes;
+using MiniECommerce.Persistence.Contexts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +16,8 @@ namespace MiniECommerce.Persistence
     {
         public static void AddPersistenceServices(this IServiceCollection services)
         {
-            services.AddSingleton<IProductService, ProductService>();
+            //services.AddSingleton<IProductService, ProductService>();
+            services.AddDbContext<MiniECommerceDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
         }
     }
 }
